8d50b6e0f3f433df8438fd3854a358468121a994
s546652 <98067009+s546652@users.noreply.github.com>
2022-03-30 13:37:14 -0500

    changes related to computation

--- routes/computation.js
+++ routes/computation.js
@@ -13,10 +13,20 @@ router.get('/', function(req, res, next) {
 
     let cbrt=Math.cbrt(x);
     res.render('computation',{title:'Computation',values:' applied to '+x+' is '+cbrt});
-    let atan2 =Math.atanh(0.5);
+    
+    let atan2 =Math.atanh(x);
     res.render('computation',{title:'Computation',values:' applied to 0.5'+' is '+atan2});
-    let atanh =Math.atan2(8, 4);
-    res.render('computation',{title:'Computation ',values:'8,4'+' is '+atanh});
+    
+    a=8;b=4;
+    let atanh =Math.atan2(a, b);
+    res.render('computation', {
+        title: 'Computation',
+        values: `applied to ` + x + ` is ` + cbrt ,
+        v1: `applied to ` + x + ` is ` + atan2,
+        v2: `applied to ` + a+b + ` is ` + atanh,
+      });
+    
+
 });
 
 module.exports = router;
